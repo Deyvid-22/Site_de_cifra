8bd84d0eb5dbd87b227c4d1c263435290db19153
DEYVID SILVA <140274792+Deyvid-22@users.noreply.github.com>
2023-08-23 14:29:06 -0300

    ajuste

--- js/list-music.js
+++ js/list-music.js
@@ -6,12 +6,10 @@ const corElements = document.querySelectorAll('.cor');
 
 //ele colocar um ouvinte de evento de click no botao
 button.addEventListener('click', clicar);
-
 let a = 12
 let b = 6
-
 // passa por cada elemento com a classe list.music
-listMusicElements.forEach(function(listMusicElement,i){
+listMusicElements.forEach((listMusicElement,i) => {
   //se o indice i for menor que b exibe o elemento 
   if(i < b){
     listMusicElements[i].style.display = 'flex'
@@ -19,18 +17,25 @@ listMusicElements.forEach(function(listMusicElement,i){
 })
 
 function clicar(){
-  listMusicElements.forEach(function(listMusicElement,i){
+  listMusicElements.forEach((listMusicElement,i) => {
     //se o indice for menor que a quando apertar o botao exibe o elemento
     if(i < a){
       listMusicElements[i].style.display = 'flex'
-      
     }
   });
   a = a + 6 
 }
-
-
-
-
-
+  //passa todos elementos da classe list.music
+listMusicElements.forEach((element,i) =>{
+  //evento do mouse ao entra na div 
+  listMusicElements[i].addEventListener('mouseover',colorir);
+  //evento do mouse ao sair da div
+  listMusicElements[i].addEventListener('mouseout', descolorir);
+  function colorir(){
+      corElements[i].style.color = '#2de627'
+  };
+  function descolorir(){
+        corElements[i].style.color = ''
+  };
+});
 
